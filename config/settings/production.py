f426d95156f41dda5fff18f74c4b84fe50ec8020
Aliaksei Pilko <aliakseipilko1@gmail.com>
2020-02-22 22:01:49 +0000

    Update requirements

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,6 +14,9 @@ DATABASES["default"] = env.db("PROD_DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
+# STATIC
+# ------------------------------------------------------------------------------
+STATIC_ROOT = '/opt/sugc/static/'
 
 # CACHES
 # ------------------------------------------------------------------------------

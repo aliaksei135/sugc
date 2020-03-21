211f608033071ad4077c01b40851f9763c6ae1a8
Aliaksei Pilko <aliakseipilko1@gmail.com>
2020-03-21 07:44:14 +0000

    Add domains

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["sugc.aliakseipilko.com", "34.67.215.153", "sugc.net"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["sugc.aliakseipilko.com", "34.67.215.153", "sugc.net",
+                                                          "www.sugc.net"])
 
 # DATABASES
 # ------------------------------------------------------------------------------

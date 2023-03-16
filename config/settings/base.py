d5e49df012814e1a8479f7f60c1af02b8c73833d
Aliaksei Pilko <a.pilko@soton.ac.uk>
2023-03-16 00:42:37 +0000

    Remove APP_DIRS loader

--- config/settings/base.py
+++ config/settings/base.py
@@ -116,7 +116,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "sugc.users.apps.UsersConfig",
     "sugc",
-    "puput"
+    # "puput"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
@@ -209,7 +209,7 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         # https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
         "DIRS": [str(APPS_DIR.path("templates"))],
-        'APP_DIRS': True,
+        # 'APP_DIRS': True,
         "OPTIONS": {
             # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
             # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
